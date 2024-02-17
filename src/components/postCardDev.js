c77cc6d855bbe10d8f16ed24514eeed1cfaa28cb
Shalivan <shalivan.inc@gmail.com>
2024-02-17 06:11:32 +0100

    *
    *

--- src/components/postCardDev.js
+++ src/components/postCardDev.js
@@ -1,9 +1,9 @@
 import React from "react"
 import { Link } from "gatsby"
-//    ZAMIAN !!!  ${props.count % 3    className={`post-card ${props.count % 4 === 0 && `post-card-large`} ${
+//    ZAMIAN !!!  ${props.count % 3 (2 w 1 lini) 
 export default props => (
   <article
-    className={`post-card ${props.count % 6 === 0  && `post-card-large` } ${
+    className={`post-card ${props.count % 6 === 0 && `post-card-large`} ${
       props.postClass
     } ${props.node.frontmatter.thumbnail ? `with-image` : `no-image`}`}
     style={
