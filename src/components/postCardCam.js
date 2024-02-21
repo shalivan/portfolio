608048b2bfb45c6d14cf34268061ac2ad1b3ffd5
Shalivan <shalivan.inc@gmail.com>
2024-02-21 22:41:03 +0100

    chaat test lage after 5

--- src/components/postCardCam.js
+++ src/components/postCardCam.js
@@ -1,25 +1,36 @@
 import React from "react"
 import { Link } from "gatsby"
-//    ZAMIAN !!!  ${props.count % 3 (2 w 1 lini) 
-export default props => (
-  <article
-    className={`post-card ${props.count % 6 === 0 && `post-card-large`} ${
-      props.postClass
-    } ${props.node.frontmatter.thumbnail ? `with-image` : `no-image`}`}
-    style={
-      props.node.frontmatter.thumbnail && {
-        backgroundImage: `url(${
-          props.node.frontmatter.thumbnail.childImageSharp.fluid.src
-        })`,
+
+export default function PostCard(props) {
+  const isLargeCard = props.count > 4;
+
+  return (
+    <article
+      className={`post-card ${isLargeCard ? 'post-card-large' : ''} ${
+        props.postClass
+      } ${props.node.frontmatter.thumbnail ? `with-image` : `no-image`}`}
+      style={
+        props.node.frontmatter.thumbnail && {
+          backgroundImage: `url(${
+            props.node.frontmatter.thumbnail.childImageSharp.fluid.src
+          })`,
+        }
       }
-    }
-  >
-    <Link to={props.node.fields.slug.split('/').slice(2, -1).join('/') === '' ? '/' : `/${props.node.fields.slug.split('/').slice(2, -1).join('/')}`} className="post-card-link">
-      <div className="post-card-content">
-        <h2 className="post-card-title">
-          {props.node.frontmatter.title }
-        </h2>
-      </div>
-    </Link>
-  </article>
-)
+    >
+      <Link
+        to={
+          props.node.fields.slug.split('/').slice(2, -1).join('/') === ''
+            ? '/'
+            : `/${props.node.fields.slug.split('/').slice(2, -1).join('/')}`
+        }
+        className="post-card-link"
+      >
+        <div className="post-card-content">
+          <h2 className="post-card-title">
+            {props.node.frontmatter.title}
+          </h2>
+        </div>
+      </Link>
+    </article>
+  )
+}
