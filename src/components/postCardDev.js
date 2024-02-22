c955729e351e8b4372a67157fddc420a2e8465ef
Shalivan <shalivan.inc@gmail.com>
2024-02-22 17:37:52 +0100

    *

--- src/components/postCardDev.js
+++ src/components/postCardDev.js
@@ -1,37 +1,5 @@
 import React from "react"
 import { Link } from "gatsby"
-//    ZAMIAN !!!  ${props.count % 3 (2 w 1 lini) 
-/*
-export default props => (
-  <article
-    className={`post-card-dev ${props.count > 4 && `post-card-large-dev`} ${
-      props.postClass
-    } ${props.node.frontmatter.thumbnail ? `with-image` : `no-image`}`}
-    style={
-      props.node.frontmatter.thumbnail && {
-        backgroundImage: `url(${
-          props.node.frontmatter.thumbnail.childImageSharp.fluid.src
-        })`,
-      }
-    }
-  >
-    <Link to={props.node.fields.slug.split('/').slice(2, -1).join('/') === '' ? '/' : `/${props.node.fields.slug.split('/').slice(2, -1).join('/')}`} className="post-card-link">
-      <div className="post-card-content">
-        <h2 className="post-card-title">
-          {props.node.frontmatter.title }
-        </h2>
-      </div>
-    </Link>
-  </article>
-)
-*/
-
-
-
-
-
-import React from "react"
-import { Link } from "gatsby"
 
 export default props => {
   let cardClass = "post-card-dev";
@@ -62,6 +30,39 @@ export default props => {
     </article>
   );
 }
+
+
+
+/* import React from "react"
+import { Link } from "gatsby"
+//    ZAMIAN !!!  ${props.count % 3 (2 w 1 lini) 
+
+export default props => (
+  <article
+    className={`post-card-dev ${props.count > 4 && `post-card-large-dev`} ${
+      props.postClass
+    } ${props.node.frontmatter.thumbnail ? `with-image` : `no-image`}`}
+    style={
+      props.node.frontmatter.thumbnail && {
+        backgroundImage: `url(${
+          props.node.frontmatter.thumbnail.childImageSharp.fluid.src
+        })`,
+      }
+    }
+  >
+    <Link to={props.node.fields.slug.split('/').slice(2, -1).join('/') === '' ? '/' : `/${props.node.fields.slug.split('/').slice(2, -1).join('/')}`} className="post-card-link">
+      <div className="post-card-content">
+        <h2 className="post-card-title">
+          {props.node.frontmatter.title }
+        </h2>
+      </div>
+    </Link>
+  </article>
+)
+*/
+
+
+
 
 
 
