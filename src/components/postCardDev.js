8044efde41df0c28ae0fa0784c97d0622b2566ec
Shalivan <shalivan.inc@gmail.com>
2024-02-23 02:44:13 +0100

    *

--- src/components/postCardDev.js
+++ src/components/postCardDev.js
@@ -3,7 +3,7 @@ import { Link } from "gatsby"
 
 export default props => {
   let cardClass = "post-card-dev";
-  if (props.count > 4) {
+  if (props.count > 2) {
     cardClass = props.count % 4 === 0 ? "post-card-home" : "post-card-pro";
   }
 
