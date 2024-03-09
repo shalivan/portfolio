c2db34d915a86a3a806faca097c711083f640a6f
Shalivan <shalivan.inc@gmail.com>
2024-03-09 23:13:42 +0100

    *

--- src/components/postCardPro.js
+++ src/components/postCardPro.js
@@ -4,7 +4,7 @@ import { Link } from "gatsby"
 //    className={`post-card ${props.count % 4 === 0 ? `post-card-large` : `` } ${
   export default props => (
     <article
-      className={`post-card ${`post-card-large-typo`} ${
+      className={`post-card ${`post-card-twosquares-min`} ${
         props.postClass
       } ${props.node.frontmatter.thumbnail ? `with-image` : `no-image`}`}
       style={
--- src/pages/pro/studio.md
+++ src/pages/pro/studio.md
@@ -1,12 +1,12 @@
 ---
-templateKey: pro-sub-page
+
 thumbnail: /img/cam/studio/DSC_0382.png
 date: 2023-03-16T04:59:59.653Z
 title: STUDIO
 description: "SORT"
 ---
 
-
+templateKey: pro-sub-page
 
 ![](/img/cam/studio/DSC_0382.png)
 
