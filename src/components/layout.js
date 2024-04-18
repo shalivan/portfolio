aaae9b0243929b04d7c2765b40a7ae3514056ad0
Shalivan <shalivan.inc@gmail.com>
2024-04-19 00:38:10 +0200

    *

--- src/components/layout.js
+++ src/components/layout.js
@@ -42,7 +42,7 @@ const Layout = props => {
           <nav id="swup" className="site-head-left">
             <ul className="nav" role="menu">
             <li className={`nav-home  ${data.pathname === '/' ? 'nav-current' : ""} `} role="menuitem">
-                <Link to={`/`}>⌂</Link>
+                <Link to={`/`}><h2>⌂</h2></Link>
               </li>
               
               <li className={`nav-home  ${data.pathname.includes('/dev') ? 'nav-current' : ""} `} role="menuitem">
