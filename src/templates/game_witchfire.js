d2329d6f55d713796e7894ec0c0e442e91d7015e
Shalivan <shalivan.inc@gmail.com>
2024-04-18 03:46:51 +0200

    *

--- src/templates/game_witchfire.js
+++ src/templates/game_witchfire.js
@@ -49,8 +49,10 @@ const GameWFPage = (props) => {
 </figure>
 
 
+<h4>Witchfire was presented on <strong>The Game Award Show 2017</strong> revealing our <strong>new IP</strong>  which is second <strong>Astronauts</strong> title. We want to maintain mystical atmosphere combined with dynamic and fun gameplay. </h5>
 
 
+<h5>Witchfire was presented on <strong>The Game Award Show 2017</strong> revealing our <strong>new IP</strong>  which is second <strong>Astronauts</strong> title. We want to maintain mystical atmosphere combined with dynamic and fun gameplay. </h5>
 
 
 <h2 id="blockquote">
